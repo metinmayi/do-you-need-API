6120563499ea58b3c51b0c72293120c93884e9f7
Metin Ahmad <metinmayi@gmail.com>
2022-08-13 01:46:07 +0200

    Added model for received user from DB

---
+++ src/models/IReceivedUser.ts
@@ -0,0 +1,12 @@
+import { RowDataPacket } from "mysql2";
+
+export interface IReceivedUser extends RowDataPacket {
+  id: number;
+  name: string;
+  password: string;
+  email: string;
+  guild: string;
+  usergroup: string;
+  createdAt: number;
+  blizz_sync: number;
+}

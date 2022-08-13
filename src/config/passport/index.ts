f13adfe38526a1896b35b29f8812f05de917746b
Metin Ahmad <metinmayi@gmail.com>
2022-08-13 20:36:39 +0200

    Changed structure of passport

--- src/config/passport/index.ts
+++ src/config/passport/index.ts
@@ -2,14 +2,17 @@ import passport from "passport";
 import { Strategy } from "passport-local";
 import bcryptjs from "bcryptjs";
 import { IReceivedUser } from "../../models/IReceivedUser";
-import { loginValidation } from "../../validations/authentication/loginValidation";
+import { isValidLogin } from "../../validations/authentication/isValidLogin";
 import { pool } from "../database/database";
 
 const sql = "SELECT name, id, password FROM users WHERE name=?";
 
+/**
+ * Login middleware
+ */
 passport.use(
   new Strategy(async (username, password, done) => {
-    if (!loginValidation(username, password)) {
+    if (!isValidLogin(username, password)) {
       return done(null);
     }
 
@@ -19,37 +22,44 @@ passport.use(
         return done(null);
       }
 
-      if (!(await matchingPasswords(password, user.password))) {
+      const isMatching = await isPasswordMatch(password, user.password);
+      if (!isMatching) {
         return done(null);
       }
 
       return done(null, user);
     } catch (error) {
       console.log(error);
-      if (!loginValidation)
-        return done(error, "Couldn't connect to the database");
+      return done(error, "Couldn't connect to the database");
     }
   })
 );
 
+/**
+ * Assign passport properties
+ */
 passport.serializeUser((user, done) => {
-  return done(null, { name: user.name, id: user.id });
+  return done(null, user.name);
 });
 
-passport.deserializeUser(async (username: Express.User, done) => {
+/**
+ * Query DB by with above passport, and return session user
+ */
+passport.deserializeUser(async (username: string, done) => {
   const sql = "SELECT * FROM users WHERE name=?";
   try {
-    const user = (
-      await pool.execute<IReceivedUser[]>(sql, [username.name])
-    )[0][0];
+    const user = (await pool.execute<IReceivedUser[]>(sql, [username]))[0][0];
     return done(null, user);
   } catch (err) {
     return done(err);
   }
 });
 
-const matchingPasswords = async (incoming: string, actual: string) => {
-  return await bcryptjs.compare(incoming, actual);
+/**
+ * Check if the requested password matches the one in the DB
+ */
+const isPasswordMatch = async (reqPassword: string, dbHash: string) => {
+  return await bcryptjs.compare(reqPassword, dbHash);
 };
 
 declare global {

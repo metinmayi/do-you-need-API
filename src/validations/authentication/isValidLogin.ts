33d5692b57f3e571eeb0d2923f99d49ba870d75d
Metin Ahmad <metinmayi@gmail.com>
2022-08-13 20:37:10 +0200

    File namechange: loginValidation -> isValidLogin

--- src/validations/authentication/isValidLogin.ts
+++ src/validations/authentication/isValidLogin.ts
@@ -5,7 +5,11 @@ const registrationSchema = Joi.object({
   username: Joi.string().pattern(USERNAME_REGEX).required(),
   password: Joi.string().pattern(PASSWORD_REGEX).required(),
 });
-export const loginValidation = (username: string, password: string) => {
+
+/**
+ * Checks if the requested username and password match the regex.
+ */
+export const isValidLogin = (username: string, password: string) => {
   const result = registrationSchema.validate({ username, password });
   if (result.error) {
     return false;

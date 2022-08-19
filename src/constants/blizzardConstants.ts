65c67938adac8559198002dd64b3ab534947c9e3
Metin Ahmad <metin.ahmad@paligo.net>
2022-08-19 23:12:36 +0200

    Updated new constants for Blizzard

--- src/constants/blizzardConstants.ts
+++ src/constants/blizzardConstants.ts
@@ -1,26 +1,36 @@
 /**
+ * Base URL for the domain
+ */
+export const BASE_URL = 'http://localhost:3000';
+
+/**
  * API Endpoint for Authorization - Oauth 2.0
  */
-export const AUTHORIZE_URL = "https://eu.battle.net/oauth/authorize";
+export const AUTHORIZE_URL = 'https://eu.battle.net/oauth/authorize';
 
 /**
  * API Endpoint for retrieving token
  */
- export const GET_TOKEN_URL = "https://eu.battle.net/oauth/token";
+export const GET_TOKEN_URL = 'https://eu.battle.net/oauth/token';
 
 /**
  * API Endpoint for checking token validation
  */
-export const CHECK_TOKEN_URL = "https://eu.battle.net/oauth/check_token"
-/**
- * World of Warcraft Region
- */
-export const WOW_REGION = 'eu';
+export const CHECK_TOKEN_URL = 'https://eu.battle.net/oauth/check_token';
 
 /**
  * The URI that blizzard redirects to after authorizing
  */
-export const REDIRECT_URI = 'http://localhost:8000/blizzard/getCharacters';
+export const REDIRECT_URI = `${BASE_URL}/blizzard/authenticateCallback`;
+
+/**
+ * The URL to send back a user to after finishing blizzard authentication
+ */
+export const SYNC_URL = `${BASE_URL}/synchronize`;
+/**
+ * World of Warcraft Region
+ */
+export const WOW_REGION = 'eu';
 
 /**
  * The scope of our battle.net request

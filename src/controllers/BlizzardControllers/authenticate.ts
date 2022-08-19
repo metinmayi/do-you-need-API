78498d85f30ed066994954bb5323c107c1e61187
Metin Ahmad <metin.ahmad@paligo.net>
2022-08-19 23:13:24 +0200

    Namechange authenticate -> getAuthorizeCode

--- src/controllers/BlizzardControllers/authenticate.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-import { AUTHORIZE_URL, REDIRECT_URI, WOW_REGION, WOW_SCOPE } from "../../constants";
-
-const clientId = process.env.BLIZZARD_CLIENT_ID || "";
-const AuthEndpoint = `${AUTHORIZE_URL}?region=${WOW_REGION}&response_type=code&client_id=${clientId}&redirect_uri=${REDIRECT_URI}&scope=${WOW_SCOPE}`;
-
-  export const authenticate = (req: Request, res: Response) => {
-        res.redirect(AuthEndpoint);
-}

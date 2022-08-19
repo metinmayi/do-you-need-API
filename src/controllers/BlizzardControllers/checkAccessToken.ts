5afeb2bc6edb7b7f4afe7958fa6a5cd4a921b7dd
Metin Ahmad <metin.ahmad@paligo.net>
2022-08-19 23:15:03 +0200

    Added controller for checking access token

--- src/controllers/BlizzardControllers/checkAccessToken.ts
+++ src/controllers/BlizzardControllers/checkAccessToken.ts
@@ -1,27 +1,24 @@
-import axios from "axios"
-import { Request, Response } from "express";
-import { CHECK_TOKEN_URL, WOW_REGION } from "../../constants";
-
+import axios from 'axios';
+import { Request, Response } from 'express';
+import { CHECK_TOKEN_URL, WOW_REGION } from '../../constants';
 
 export const checkAccessToken = async (req: Request, res: Response) => {
-    debugger;
-    const token = req.user?.accessToken;
+  const token = req.user?.accessToken;
 
-    if (typeof token !== 'string') {
-        res.sendStatus(400);
-        return;
-    }
+  if (typeof token !== 'string') {
+    res
+      .status(401)
+      .json({ message: 'User does not have an active accessToken' });
+    return;
+  }
 
-    const result = await axios(CHECK_TOKEN_URL,{
-        method: 'POST',
-        headers: {
-            'Content-Type' : 'application/x-www-form-urlencoded'
-        },
-        data: `region=${WOW_REGION}&token=${token}`
-    });
-    console.log(result);
-    
-    // if response is good, sendStatus(200)
-    // else sendStatus(401);
-}
+  const result = await axios(CHECK_TOKEN_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    data: `region=${WOW_REGION}&token=${token}`,
+  });
 
+  res.sendStatus(result.status);
+};

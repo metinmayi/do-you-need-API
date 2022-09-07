5c307889a7e4df57d308777fb29d22ad033bf91d
Metin Ahmad <metinmayi@gmail.com>
2022-09-08 00:03:44 +0200

    getGuild now uses standard DYNResponse

--- src/controllers/blizzardController/getGuild.ts
+++ src/controllers/blizzardController/getGuild.ts
@@ -1,14 +1,19 @@
 import { Request, Response } from "express";
 import { getPlayer } from "../../helpers/blizzardHelpers/getPlayer";
 import { dbGuildStatus } from "../../helpers/doYouNeedHelpers/dbGuildStatus";
+import { DYNResponse } from "../../models/DYNResponse";
 import { getGuildStatusValidation } from "../../validations/blizzardValidation/getGuildStatusValidation";
+import { getUnregisteredGuild } from "./getUnregisteredGuild";
 
 export const getGuild = async (req: Request, res: Response) => {
+  const response = new DYNResponse();
   const token = req.user?.accessToken;
 
   const validation = getGuildStatusValidation(req.query, token);
   if (!validation.success) {
-    return res.status(403).json(validation.error);
+    response.error = true;
+    response.errorMessage = validation.error.message;
+    return res.status(403).json(response);
   }
 
   try {
@@ -17,10 +22,18 @@ export const getGuild = async (req: Request, res: Response) => {
       validation.data.realm,
       validation.data.token
     );
-    const guild = await dbGuildStatus(player.guild.id);
+
+    const guild =
+      (await dbGuildStatus(player.guild.id)) || getUnregisteredGuild(player);
+
+    response.message = "No registered guilds were found";
+    response.data.push(guild);
+
     res.status(200).json(guild);
   } catch (error: any) {
+    response.error = true;
+    response.errorMessage = error.message;
     console.log("getGuildStatus" + error.message);
-    res.status(500).send(error.message);
+    res.status(500).json(response);
   }
 };

28707b7b377d8782c592c2c774e8414f57309c8e
Metin Ahmad <metin.ahmad@paligo.net>
2022-09-24 10:30:26 +0200

    Updated validation for registerGuild

--- src/validations/blizzardValidation/registerGuildValidation.ts
+++ src/validations/blizzardValidation/registerGuildValidation.ts
@@ -1,13 +1,28 @@
 import { z } from "zod";
+import { INewGuild } from "../../models/INewGuild";
 
-const zRegisterGuildSchema = z
-  .object({
+interface IRegisterGuild {
+  guild: INewGuild;
+  character: string;
+  token: string;
+  realm: string;
+}
+
+const zRegisterGuildSchema: z.ZodType<IRegisterGuild> = z.lazy(() =>
+  z.object({
     character: z.string(),
-    guild: z.string(),
-    realm: z.string(),
     token: z.string(),
+    realm: z.string(),
+    guild: z.object({
+      isNew: z.boolean(),
+      id: z.string(),
+      license: z.string(),
+      name: z.string(),
+      realm: z.string(),
+      faction: z.string(),
+    }),
   })
-  .strict();
+);
 
 type zRegisterGuildSchema = z.infer<typeof zRegisterGuildSchema>;
 export const registerGuildValidation = (user: any) => {

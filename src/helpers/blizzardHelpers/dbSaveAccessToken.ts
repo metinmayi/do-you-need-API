38a92d699c109f4d86488e76366f5ebab72cb55d
Metin Ahmad <metin.ahmad@paligo.net>
2022-09-24 11:05:23 +0200

    Annotation

--- src/helpers/blizzardHelpers/dbSaveAccessToken.ts
+++ src/helpers/blizzardHelpers/dbSaveAccessToken.ts
@@ -1,6 +1,11 @@
 import { ObjectId } from "mongoose";
 import { UserModel } from "../../mongoose/schemas/UserSchema";
 
+/**
+ * Saves an accesstoken to the user in the database
+ * @param accessToken Newly retrieved accessToken from Blizzard's API.
+ * @param id ID of the user in the DB
+ */
 export const dbSaveAccessToken = async (accessToken: string, id: ObjectId) => {
   const _id = id.toString();
   await UserModel.updateOne({ _id }, { accessToken });

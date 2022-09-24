d263ed93ebc1d68601be9e121787d9ca6a3481c8
Metin Ahmad <metin.ahmad@paligo.net>
2022-09-24 11:04:36 +0200

    Added annotation

--- src/helpers/blizzardHelpers/dbAddGuildToUser.ts
+++ src/helpers/blizzardHelpers/dbAddGuildToUser.ts
@@ -2,6 +2,12 @@ import { IGuild } from "../../models/IGuild";
 import { IUserGuild } from "../../models/IUserGuild";
 import { UserModel } from "../../mongoose/schemas/UserSchema";
 
+/**
+ * Inserts a guild to the list of player's guilds.
+ * @param username DoYouNeed username
+ * @param guild Guild object
+ * @param playerRank Rank of the player
+ */
 export async function dbAddGuildToUser(
   username: any,
   guild: IGuild,

cab9fbd3e142917b7101d62bef052f70c7cf2d8f
Metin Ahmad <metin.ahmad@paligo.net>
2022-09-28 19:31:11 +0200

    Password regex now depends on the ENVIRONMENT

--- src/constants/authentication.ts
+++ src/constants/authentication.ts
@@ -1,3 +1,7 @@
+import { ENVIRONMENT } from "./config";
+
 export const USERNAME_REGEX = /^[a-zA-Z][a-zA-Z0-9_]{3,23}$/;
 export const PASSWORD_REGEX =
-  /.(?=.*[a-zA-Z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@"#$%]).{8,24}$/;
+  ENVIRONMENT === "production"
+    ? /.(?=.*[a-zA-Z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@"#$%]).{8,24}$/
+    : /./;

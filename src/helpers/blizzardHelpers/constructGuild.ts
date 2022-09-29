4aab37ce64a62a36c265c77b4103a643cd0b94be
Metin Ahmad <metin.ahmad@paligo.net>
2022-09-29 18:13:05 +0200

    Added helper: constructGuild

---
+++ src/helpers/blizzardHelpers/constructGuild.ts
@@ -0,0 +1,13 @@
+import { BlizzardRetrievedGuild } from "../../models/BlizzardModels/BlizzardRetrievedGuild";
+import { IGuild } from "../../models/IGuild";
+
+export function constructGuild(guildInformation: BlizzardRetrievedGuild) {
+  const guild: IGuild = {
+    id: guildInformation.id.toString(),
+    name: guildInformation.name,
+    realm: guildInformation.realm.slug,
+    license: "standard",
+    faction: guildInformation.faction.type,
+  };
+  return guild;
+}

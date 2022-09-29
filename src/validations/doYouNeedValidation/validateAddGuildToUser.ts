2d1b68e0e1d7faab0d6c4d0b27c858da62edee24
Metin Ahmad <metin.ahmad@paligo.net>
2022-09-29 18:12:39 +0200

    Added validation for addGuildToUser

---
+++ src/validations/doYouNeedValidation/validateAddGuildToUser.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+import { IGuild } from "../../models/IGuild";
+
+interface IAddGuildToUser {
+  guild: IGuild;
+  token: string;
+}
+
+const zAddGuildToUserSchema: z.ZodType<IAddGuildToUser> = z.lazy(() =>
+  z.object({
+    guild: z.object({
+      id: z.string(),
+      name: z.string(),
+      realm: z.string(),
+      license: z.string(),
+      faction: z.string(),
+    }),
+    token: z.string(),
+  })
+);
+
+export function zValidateAddGuildToUser(request: any) {
+  const parsed = zAddGuildToUserSchema.safeParse(request);
+  return parsed;
+}

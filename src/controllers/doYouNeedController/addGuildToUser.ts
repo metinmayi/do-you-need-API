2113c7a23c568f9e161da28b2229c30b877f14b8
Metin Ahmad <metin.ahmad@paligo.net>
2022-09-29 19:38:38 +0200

    Finished controller for dbAddGuildToUser

--- src/controllers/doYouNeedController/addGuildToUser.ts
+++ src/controllers/doYouNeedController/addGuildToUser.ts
@@ -1,9 +1,28 @@
 import { Request, Response } from "express";
+import { dbAddGuildToUser } from "../../helpers/blizzardHelpers/dbAddGuildToUser";
+import { zValidateAddGuildToUser } from "../../validations/doYouNeedValidation/validateAddGuildToUser";
 
 export async function addGuildToUser(req: Request, res: Response) {
-  console.log("reached");
-  // Check that we're authenticated
-  // Validate request
-  // Attempt to add the guild to the user
+  try {
+    // Validate request
+    const validation = zValidateAddGuildToUser({
+      guild: req.body,
+      token: req.user?.accessToken,
+    });
+    if (!validation.success) {
+      return res.status(403).json(validation.error.errors);
+    }
+
+    if (!req.user?._id) {
+      return;
+    }
+
+    await dbAddGuildToUser(req.user?._id, validation.data.guild);
+    return res.sendStatus(200);
+  } catch (error: any) {
+    res.sendStatus(500);
+    console.log(error);
+  }
+
   // Return
 }

251ef349ecf54b5ab97631592edadb6ae8c33a19
Metin Ahmad <metin.ahmad@paligo.net>
2022-09-29 15:31:54 +0200

    Started controller for addGuildToUser

---
+++ src/controllers/doYouNeedController/addGuildToUser.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+export async function addGuildToUser(req: Request, res: Response) {
+  console.log("reached");
+  // Check that we're authenticated
+  // Validate request
+  // Attempt to add the guild to the user
+  // Return
+}

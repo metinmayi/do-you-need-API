f58b87bb57fc4fcdb50e5228375a2fb46e428705
Metin Ahmad <metin.ahmad@paligo.net>
2022-09-30 18:35:24 +0200

    Updated some constants and created type predicates.

--- src/constants/instanceConstants.ts
+++ src/constants/instanceConstants.ts
@@ -1,7 +1,17 @@
 /**
+ * Current raid ID
+ */
+export const RaidId = 1195 as const;
+
+/**
+ * Supported Raid Difficulty
+ */
+export const RaidDifficulty = "raid-mythic-fated" as const;
+
+/**
  * Give ID, get bossname
  */
-export const IdToBoss = {
+export const IdToBoss: Record<number, string> = {
   2458: "vigilant_guardian",
   2465: "skolex_the_insatiable_ravener",
   2470: "artificer_xymox",
@@ -13,46 +23,35 @@ export const IdToBoss = {
   2457: "lords_of_dread",
   2467: "rygelon",
   2464: "the_jailer",
-};
+} as const;
 
 /**
- * Sepulcher boss Ids
+ * Array of bosses in the raid
  */
-export type IBossId =
-  | "2458"
-  | "2465"
-  | "2470"
-  | "2459"
-  | "2460"
-  | "2461"
-  | "2463"
-  | "2469"
-  | "2457"
-  | "2467"
-  | "2464";
+const BOSS_NAMES = [
+  "vigilant_guardian",
+  "skolex_the_insatiable_ravener",
+  "artificer_xymox",
+  "dausegne_the_fallen_oracle",
+  "prototype_pantheon",
+  "lihuvim_principal_architect",
+  "halondrus_the_reclaimer",
+  "anduin_wrynn",
+  "lords_of_dread",
+  "rygelon",
+  "the_jailer",
+] as const;
 
 /**
- * Sepulcher boss Ids
+ * Boss names inherited by the array of bosses in the raid
  */
-export type IBossName =
-  | "vigilant_guardian"
-  | "skolex_the_insatiable_ravener"
-  | "artificer_xymox"
-  | "dausegne_the_fallen_oracle"
-  | "prototype_pantheon"
-  | "lihuvim_principal_architect"
-  | "halondrus_the_reclaimer"
-  | "anduin_wrynn"
-  | "lords_of_dread"
-  | "rygelon"
-  | "the_jailer";
+export type IBossName = typeof BOSS_NAMES[number];
 
 /**
- * Current raid ID
+ * Type predicate to check if the string is a IBossName
+ * @param {string} name Any string to test
  */
-export const RaidId = 1195 as const;
-
-/**
- * Supported Raid Difficulty
- */
-export const RaidDifficulty = "raid-mythic-fated" as const;
+export function isIBossName(name: string): name is IBossName {
+  const arr: any[] = [...BOSS_NAMES];
+  return arr.includes(name);
+}

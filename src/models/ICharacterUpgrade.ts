a5311617752c2fce0423e4f8f0ea6a788360f70e
Metin Ahmad <metin.ahmad@paligo.net>
2022-09-30 07:58:01 +0200

    ICharacterUpgrade is now an interface with slightly looser types

--- src/models/ICharacterUpgrade.ts
+++ src/models/ICharacterUpgrade.ts
@@ -1,44 +1,8 @@
 export interface ICharacterUpgrade {
   playerName: string;
   selected: boolean;
-  role: "tank" | "healing" | "dps";
-  className:
-    | "warrior"
-    | "mage"
-    | "paladin"
-    | "hunter"
-    | "rogue"
-    | "priest"
-    | "shaman"
-    | "warlock"
-    | "monk"
-    | "druid"
-    | "demonHunter"
-    | "deathKnight";
+  role: string;
+  className: string;
   playerUpgrades: any[];
-  guildName: string;
   upgradeCount?: number;
 }
-
-// export interface IUPgradeItem {
-//   itemSlot: string;
-//   dpsPercentage: number;
-//   dpsNumber: number;
-// }
-
-// export type IItemType =
-//   | "head"
-//   | "neck"
-//   | "shoulder"
-//   | "back"
-//   | "chest"
-//   | "wrist"
-//   | "hands"
-//   | "waist"
-//   | "legs"
-//   | "feet"
-//   | "ring"
-//   | "trinket"
-//   | "mainHand"
-//   | "offHand"
-//   | "oneHand"

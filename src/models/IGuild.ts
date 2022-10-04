6dbb9e41d1227ed1b0803e8aa18d6850eead867a
Metin Ahmad <metin.ahmad@paligo.net>
2022-10-04 14:21:10 +0200

    Added type predicate for IGuild

--- src/models/IGuild.ts
+++ src/models/IGuild.ts
@@ -8,3 +8,15 @@ export interface IGuild {
   faction: string;
   characters: ICharacter[];
 }
+
+export function isIGuild(object: any): object is IGuild {
+  if (typeof object !== "object" || !object) return false;
+  return (
+    typeof object.id === "string" &&
+    typeof object.name === "string" &&
+    typeof object.realm === "string" &&
+    typeof object.license === "string" &&
+    typeof object.faction === "string" &&
+    typeof object.characters === "object"
+  );
+}

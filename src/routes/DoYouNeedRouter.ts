5e60c7c30143226c6b288c242382b835b981ff63
Metin Ahmad <metin.ahmad@paligo.net>
2022-10-04 14:13:57 +0200

    Added router for getGuildCharacters

--- src/routes/DoYouNeedRouter.ts
+++ src/routes/DoYouNeedRouter.ts
@@ -1,13 +1,12 @@
 /**
- * Routes for domain.com/player/
+ * Routes for domain.com/doyouneed/
  */
 import express from "express";
 const DoYouNeedRouter = express.Router();
 import { Request, Response } from "express";
 import { addCharacterUpgrades } from "../controllers/doYouNeedController/addCharacterUpgrades";
 import { addGuildToUser } from "../controllers/doYouNeedController/addGuildToUser";
-import { getBossTable } from "../controllers/doYouNeedController/getBossTable";
-import { getBossTableValidation } from "../validations/doYouNeedValidation/getBossTableValidation";
+import { getGuildCharacterUpgrades } from "../controllers/doYouNeedController/getGuildCharacters";
 
 DoYouNeedRouter.get("/", (req: Request, res: Response) => {
   res.send("Reached DoYouNeedRouter");
@@ -22,17 +21,9 @@ DoYouNeedRouter.post("/addCharacterUpgrades", addCharacterUpgrades);
  * Route for adding a guild to a user's guilds.
  */
 DoYouNeedRouter.post("/addGuildToUser", addGuildToUser);
+
 /**
  * Gets player information from a boss table
  */
-DoYouNeedRouter.get("/getBossTable/", async (req: Request, res: Response) => {
-  // Validate bossName
-  const isValidated = getBossTableValidation(req.query.bossName?.toString());
-  if (!isValidated) {
-    return res.status(400).send("Invalid boss");
-  }
-
-  const result = await getBossTable("the_jailer");
-  res.status(200).send(result?.[0]);
-});
+DoYouNeedRouter.post("/getGuildCharacters", getGuildCharacterUpgrades);
 export default DoYouNeedRouter;

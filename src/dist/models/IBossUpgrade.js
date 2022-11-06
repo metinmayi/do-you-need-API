2d73cad7bd40091fced9a2ca0efdc2dfd59a68db
Metin Ahmad <metin.ahmad@paligo.net>
2022-11-06 22:12:12 +0100

    Updated class IBossUpgrade

--- src/dist/models/IBossUpgrade.js
+++ src/dist/models/IBossUpgrade.js
@@ -1,2 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.IBossUpgrade = void 0;
+class IBossUpgrade {
+    constructor() {
+        this.boss_name = "";
+        this.character_id = "";
+        this.head = null;
+        this.shoulder = null;
+        this.chest = null;
+        this.wrist = null;
+        this.hands = null;
+        this.waist = null;
+        this.legs = null;
+        this.feet = null;
+        this.neck = null;
+        this.back = null;
+        this.finger = null;
+        this.trinket = null;
+        this.main_hand = null;
+        this.one_hand = null;
+        this.off_hand = null;
+    }
+}
+exports.IBossUpgrade = IBossUpgrade;
--- src/models/IBossUpgrade.ts
+++ src/models/IBossUpgrade.ts
@@ -1,4 +1,19 @@
-export interface IBossUpgrade {
-    dpsPercentage: string;
-    dpsUpgrade: string;
+export class IBossUpgrade {
+  boss_name = "";
+  character_id = "";
+  head = null;
+  shoulder = null;
+  chest = null;
+  wrist = null;
+  hands = null;
+  waist = null;
+  legs = null;
+  feet = null;
+  neck = null;
+  back = null;
+  finger = null;
+  trinket = null;
+  main_hand = null;
+  one_hand = null;
+  off_hand = null;
 }

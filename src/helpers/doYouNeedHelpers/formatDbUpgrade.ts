839e9d15c0aad0029f0331a9c8e0ebfccbe92881
Metin Ahmad <metin.ahmad@paligo.net>
2022-11-08 21:35:10 +0100

    Created function that formats upgrades from the DB

---
+++ src/helpers/doYouNeedHelpers/formatDbUpgrade.ts
@@ -0,0 +1,33 @@
+import { bossLoot } from "../../constants/bossLoot";
+
+/**
+ * Takes an array of upgrades and formats them in accordance to client requirements.
+ * @param upgrades Array of upgrades from the DB.
+ * @returns Array of upgrades formatted to client needs.
+ */
+export async function formatDbUpgrade(upgrades: any[], bossName: string) {
+  const formattedUpgrades = upgrades.map((upgrade: any) => {
+    const upgradeObject = {
+      id: upgrade.id,
+      selected: upgrade.selected,
+      bossName: upgrade.boss_name,
+      name: upgrade.name,
+      role: upgrade.role,
+      class: upgrade.class,
+      upgrades: [""],
+    };
+
+    const bossDrops = bossLoot[bossName];
+    bossDrops.forEach((bossDrop) => {
+      let incomingUpgrade = upgrade[bossDrop];
+      if (!incomingUpgrade) {
+        incomingUpgrade = "-";
+      }
+      upgradeObject.upgrades.push(incomingUpgrade);
+    });
+
+    return upgradeObject;
+  });
+
+  return formattedUpgrades;
+}
